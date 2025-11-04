48320d10bc71f620832038e110a7e6fdf3b9fbf9
aquib2502 <aquibhingwala@gmail.com>
2025-11-04 22:36:28 +0530

    Suspense boundry

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import Dashboard from '@/components/Dashboard/Dashboard'
 const page = () => {
   return (
     <div>
+      <Suspense fallback={<div>Loading...</div>}>
         <Dashboard  />
+      </Suspense>
     </div>
   )
 }
