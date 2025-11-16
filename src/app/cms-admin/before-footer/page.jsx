24259a0cce9b213ec967cd9ab6fd88492b74a35b
noumankhan20 <noumankhan8097@gmail.com>
2025-11-16 21:37:36 +0530

    feat: add StartFundraiserBannerCMS component for managing fundraiser banner content added before footer section

---
+++ src/app/cms-admin/before-footer/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import StartFundraiserBannerCMS from '@/components/CMS-Admin/BeforeFooter';
+const page = () => {
+  return (
+    <div>
+        <StartFundraiserBannerCMS />
+        </div>
+  )
+}
+
+export default page
