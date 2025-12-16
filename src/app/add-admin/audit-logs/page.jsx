c3cc30f3130352557d2c35a666287c0a1af80def
aquib2502 <aquibhingwala@gmail.com>
2025-12-16 22:08:04 +0530

    Audit Logs error build

--- src/app/add-admin/audit-logs/page.jsx
+++ src/app/add-admin/audit-logs/page.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
 import AuditLogs from '@/components/AddAdmin/AuditLogs'
+
+import React from 'react'
 const page = () => {
   return (
     <div>
-        <AuditLogs />
-    </div>
+      <AuditLogs />
+      </div>
   )
 }
 
