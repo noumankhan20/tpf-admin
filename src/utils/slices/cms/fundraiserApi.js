d14d19bd9f84971e2e37e63ef7427501157cebdb
noumankhan20 <noumankhan8097@gmail.com>
2025-12-27 14:38:54 +0530

    feat: create fundraiserApi.js

---
+++ src/utils/slices/cms/fundraiserApi.js
@@ -0,0 +1,49 @@
+import { apiSlice } from "../apiSlice";
+
+export const fundraiserApi = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+
+    // 🔹 GET ALL FUNDRAISERS
+    getFundraisers: builder.query({
+      query: () => "/cms/fundraiser/get",
+      providesTags: ["Fundraiser"],
+    }),
+
+    // 🔹 CREATE FUNDRAISER
+    createFundraiser: builder.mutation({
+      query: (formData) => ({
+        url: "/cms/fundraiser/add",
+        method: "POST",
+        body: formData,
+      }),
+      invalidatesTags: ["Fundraiser"],
+    }),
+
+    // 🔹 UPDATE FUNDRAISER
+    updateFundraiser: builder.mutation({
+      query: ({ id, formData }) => ({
+        url: `/cms/fundraiser/update/${id}`,
+        method: "PUT",
+        body: formData,
+      }),
+      invalidatesTags: ["Fundraiser"],
+    }),
+
+    // 🔹 DELETE FUNDRAISER
+    deleteFundraiser: builder.mutation({
+      query: (id) => ({
+        url: `/cms/fundraiser/delete/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Fundraiser"],
+    }),
+
+  }),
+});
+
+export const {
+    useGetFundraisersQuery,
+    useCreateFundraiserMutation,
+    useUpdateFundraiserMutation,
+    useDeleteFundraiserMutation,
+} = fundraiserApi;
